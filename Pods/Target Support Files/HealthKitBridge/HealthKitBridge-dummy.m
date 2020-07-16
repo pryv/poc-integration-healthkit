5360020f3d92cea5699bc3eebde467aee97a4846
Sara Alemanno <sara.alemanno@epfl.ch>
2020-07-16 13:51:53 +0200

    Update .gitignore

--- Pods/Target Support Files/HealthKitBridge/HealthKitBridge-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_HealthKitBridge : NSObject
-@end
-@implementation PodsDummy_HealthKitBridge
-@end
--- Pods/Target Support Files/PryvSwiftKit/PryvSwiftKit-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PryvSwiftKit : NSObject
-@end
-@implementation PodsDummy_PryvSwiftKit
-@end
